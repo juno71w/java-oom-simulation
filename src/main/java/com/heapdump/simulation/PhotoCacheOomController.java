bb3d6c22eea342c02f6727ed9601678d32b2409f
박준호 <qkrwnsgh71w@gmail.com>
2025-12-03 11:58:04 +0900

    feat. OOM 시뮬레이션 코드 추가

---
+++ src/main/java/com/heapdump/simulation/PhotoCacheOomController.java
@@ -0,0 +1,28 @@
+package com.heapdump.simulation;
+
+import org.mycache.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+@RequestMapping("/simulation/photo-cache")
+public class PhotoCacheOomController {
+
+    private static final Random RANDOM = new Random();
+    private static final MyCache myCache = new MyCache();
+    private static final int ONE_HUNDRED_MB = 10 * 1024 * 1024;
+
+    @GetMapping("/oom")
+    public String simulatePhotoCacheOom(@RequestParam(name = "id") long id) {
+
+        byte[] photo = new byte[ONE_HUNDRED_MB];
+        RANDOM.nextBytes(photo);
+        myCache.put(id, photo);
+
+        return "캐시에 저장 성공!!";
+    }
+}
